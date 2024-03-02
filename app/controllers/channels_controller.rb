84f81c5ea44d2ea5d112013496a7ffa9bf27d862
sachagigoi <156658359+sachagigoi@users.noreply.github.com>
2024-03-02 16:48:25 +0100

    controllers

---
+++ app/controllers/channels_controller.rb
@@ -0,0 +1,23 @@
+class ChannelsController < ApplicationController
+  def index
+    @channels = Channel.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
